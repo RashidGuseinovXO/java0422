6e61ac2e8a3a15f028c119cdadf13d3c73801504
rasidgusejnov <rishson@mail.ru>
2022-05-05 19:11:47 +0300

    Homework done

--- src/main/java/r/guseynov/homework1/HomeWorkApp.java
+++ src/main/java/r/guseynov/homework1/HomeWorkApp.java
@@ -2,12 +2,48 @@ package r.guseynov.homework1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
 
     }
-    // task 2
+      // Task 2
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
+     // Task 3
+    public static void checkSumSign() {
+        int a = 7, b = 8;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+     // Task 4
+    public static void printColor() {
+        int value = 11;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value >= 0) {
+            System.out.println("Желтый");
+        } else if (value >= 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 11;
+        int b = 9;
+        if (a >= b) {
+            System.out.println("a >= b");
+            
+        }
+
+    }
 }
+
