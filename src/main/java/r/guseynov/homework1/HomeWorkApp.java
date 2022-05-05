782630f012107614a65f1a042bd0ae879499a7e5
rasidgusejnov <rishson@mail.ru>
2022-05-05 18:31:49 +0300

    OK

--- src/main/java/r/guseynov/homework1/HomeWorkApp.java
+++ src/main/java/r/guseynov/homework1/HomeWorkApp.java
@@ -4,4 +4,10 @@ public class HomeWorkApp {
     public static void main(String[] args) {
 
     }
+    // task 2
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
 }
